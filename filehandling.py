152bdad105fb1e92d9da0bcadef9172196431514
Daniela Sanchez <techdaniela@gmail.com>
2023-01-19 12:28:51 -0600

    adding scripts

---
+++ filehandling.py
@@ -0,0 +1,12 @@
+
+
+
+with open('words', 'r') as infile:
+    for _ in range(5):
+        print(infile.readline().strip('\n'))
+
+
+
+
+
+
---
+++ randomnames.py
@@ -0,0 +1,8 @@
+# this is a program that generates random names from the 'names' file
+
+import names
+
+for _ in range(5):
+    print(names.get_full_name())
+
+
